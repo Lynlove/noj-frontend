7cffd839cff4df66d6b3f788224af44222cdf988
lyn <3228562873@qq.com>
2024-06-16 16:32:14 +0800

    引入了文本编辑器、代码编辑器，完成题目创建页面、题目更新页面、题目管理页面。

---
+++ src/generated/models/QuestionSubmitVO.ts
@@ -0,0 +1,21 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { JudgeInfo } from './JudgeInfo';
+import type { QuestionVO } from './QuestionVO';
+import type { UserVO } from './UserVO';
+export type QuestionSubmitVO = {
+    code?: string;
+    createTime?: string;
+    id?: number;
+    judgeInfo?: JudgeInfo;
+    language?: string;
+    question?: QuestionVO;
+    questionId?: number;
+    status?: number;
+    updateTime?: string;
+    user?: UserVO;
+    userId?: number;
+};
+
